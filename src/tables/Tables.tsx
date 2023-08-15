af2c48c50935620f5e9cdf98420da1c78500819f
Towhid <towhidtayeb10@gmail.com>
2023-08-15 08:59:14 +0200

    saved

--- src/tables/Tables.tsx
+++ src/tables/Tables.tsx
@@ -103,7 +103,10 @@ export const Tables = () => {
         return [data, setData] as const;
     };
 
-    const [rows1, setRows1] = useLocalStorageStateTable1('tableRows1', createInitialRowsTable1(11, true));
+    //const [rows1, setRows1] = useLocalStorageStateTable1('tableRows1', createInitialRowsTable1(11, true));
+    const storedRows1 = localStorage.getItem('tableRows1');
+    const initialRows1 = storedRows1 ? JSON.parse(storedRows1) : createInitialRowsTable1(11, true);
+    const [rows1, setRows1] = useLocalStorageStateTable1('tableRows1', initialRows1);
 
     const handleInputChange1 = (
         e: React.ChangeEvent<HTMLTextAreaElement>,
@@ -111,7 +114,10 @@ export const Tables = () => {
         column: keyof RowData1
     ) => {
         const { value } = e.target;
-        setRows1((prevRows) => prevRows.map((row) => (row.id === id ? { ...row, [column]: value } : row)));
+        const updatedRows = rows1.map((row) => (row.id === id ? { ...row, [column]: value } : row));
+        setRows1(updatedRows);
+        localStorage.setItem('tableRows1', JSON.stringify(updatedRows));
+        //setRows1((prevRows) => prevRows.map((row) => (row.id === id ? { ...row, [column]: value } : row)));
     };
 
     // const formatDate1 = (date: Date) => date.toLocaleDateString();
@@ -314,7 +320,6 @@ export const Tables = () => {
                             fontStyle: 'gerade',
                             fontSize: '20px',
                             color: 'blue',
-                            textShadow: '2px 2px 15px red',
                             padding: '0px 10px',
                             width: '95px',
                             height: '90px',
@@ -323,7 +328,7 @@ export const Tables = () => {
                         Note:
                     </div>
                     Zum Editieren einfach in eine Zelle klicken & Text eintragen.<br/>
-                    Diese Website ist ein Geschenk an die Familie Notfall ♥️.
+                    Diese Website ist ein Geschenk an die Familie Notfall ♥️.<br/>
                 </p>
 
 
